6ff493ba80ba4dce4f2ec204ba3405f62a1a5bd8
Caroline Vieira <65242939+Ca-byte@users.noreply.github.com>
2023-07-07 23:44:26 +0200

    feat: Application interface - Radix Collapsible in modules

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -1,3 +1,4 @@
+import * as Collapsible from '@radix-ui/react-collapsible';
 import { ChevronDown} from "lucide-react";
 import { Lesson } from "./Lesson";
 
@@ -9,8 +10,8 @@ interface ModuleProps{
 
 export function Module({title, moduleIndex, amountOfLessons}: ModuleProps){
 	return(
-		<div>
-			<button className="flex w-full items-center  gap-3 bg-zinc-800 p-4">
+		<Collapsible.Root className="group">
+			<Collapsible.Trigger className="flex w-full items-center  gap-3 bg-zinc-800 p-4">
 				<div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs">
 					{moduleIndex + 1}
 				</div>
@@ -20,13 +21,15 @@ export function Module({title, moduleIndex, amountOfLessons}: ModuleProps){
 					</strong>
 					<span className="text-xs text-zinco-400">{amountOfLessons}classes</span>
 				</div>
-				<ChevronDown className="w-5 h-5 ml-auto text-zinc-400" />
-			</button>
-			<nav className="relative flex flex-col gap-4 p-6">
-				<Lesson title="Redux fundamentals" duration="06:09"/>
-				<Lesson title="Zustand fundamentals" duration="08:11"/>
-				<Lesson title="Jatai fundamentals" duration="07:07"/>
-			</nav>
-		</div>
+				<ChevronDown className="w-5 h-5 ml-auto text-zinc-400group-data-[state=open]:rotate-180 transition-transform" />
+			</Collapsible.Trigger>
+			<Collapsible.Content>
+				<nav className="relative flex flex-col gap-4 p-6">
+					<Lesson title="Redux fundamentals" duration="06:09"/>
+					<Lesson title="Zustand fundamentals" duration="08:11"/>
+					<Lesson title="Jatai fundamentals" duration="07:07"/>
+				</nav>
+			</Collapsible.Content>
+		</Collapsible.Root>
 	)
 }
