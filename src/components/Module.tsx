99b9ee06a6e0dfe3ef2fef42b53ba1564a25eff2
Caroline Vieira <65242939+Ca-byte@users.noreply.github.com>
2023-07-10 20:45:16 +0200

    feat: Application interface - Radix Collapsible in modules - group-data-state

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -21,7 +21,7 @@ export function Module({title, moduleIndex, amountOfLessons}: ModuleProps){
 					</strong>
 					<span className="text-xs text-zinco-400">{amountOfLessons}classes</span>
 				</div>
-				<ChevronDown className="w-5 h-5 ml-auto text-zinc-400group-data-[state=open]:rotate-180 transition-transform" />
+				<ChevronDown className="w-5 h-5 ml-auto text-zinc-400 group-data-[state=open]:rotate-180 transition-transform" />
 			</Collapsible.Trigger>
 			<Collapsible.Content>
 				<nav className="relative flex flex-col gap-4 p-6">
