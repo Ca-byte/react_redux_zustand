b348f6433afe3a40fc036de12a352ef6be7f0659
Caroline Vieira <65242939+Ca-byte@users.noreply.github.com>
2023-07-11 22:05:13 +0200

    feat: Redux Aplication - Select current class video

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -2,6 +2,8 @@ import * as Collapsible from '@radix-ui/react-collapsible';
 import { ChevronDown} from "lucide-react";
 import { Lesson } from "./Lesson";
 import { useAppSelector } from '../store';
+import { useDispatch } from 'react-redux';
+import { play } from '../store/Slice/player';
 
 interface ModuleProps{
 	title: string
@@ -10,6 +12,8 @@ interface ModuleProps{
 }
 
 export function Module({title, moduleIndex, amountOfLessons}: ModuleProps){
+	const dispatch = useDispatch()
+
 	const lessons = useAppSelector((state)=> {
 		return state.player.course.modules[moduleIndex].lessons
 
@@ -30,12 +34,14 @@ export function Module({title, moduleIndex, amountOfLessons}: ModuleProps){
 			</Collapsible.Trigger>
 			<Collapsible.Content>
 				<nav className="relative flex flex-col gap-4 p-6">
-					{lessons.map((lesson)=>{
+					{lessons.map((lesson, lessonIndex)=>{
 						return (
 							<Lesson 
-							key={lesson.id}
-							title={lesson.title} 
-							duration={lesson.duration}/>
+								key={lesson.id}
+								title={lesson.title} 
+								duration={lesson.duration}
+								onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
+							/>
 						)
 					})}
 				</nav>
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,6 +1,13 @@
 import ReactPlayer from "react-player";
+import { useAppSelector } from "../store";
 
 export function Video(){
+	const lesson = useAppSelector(state => {
+		const { currentLessonIndex, currentModuleIndex} = state.player
+
+		const currentLesson = state.player.course.modules[currentModuleIndex].lessons[currentLessonIndex]
+		return currentLesson
+	})
 	return(
 		<div className="flex-1">
 			<div className="w-full bg-zinc-950 aspect-video">
@@ -8,7 +15,7 @@ export function Video(){
 				width="100%"
 				height="100%"
 				controls
-				url="https://www.youtube.com/watch?v=u99tNt3TZf8"
+				url={`https://www.youtube.com/watch?v=${lesson.id}`}
 				/>
 			</div>
 		</div>
